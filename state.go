52b866c8bd6366dcdba138ddacaf314a745395eb
wreulicke <masaya2dies@gmail.com>
2022-05-13 02:08:51 +0900

    separate file

--- state.go
+++ state.go
@@ -1,66 +1,10 @@
 package circuitbreaker
 
-import (
-	"time"
-
-	"github.com/benbjohnson/clock"
-)
-
 const (
 	StateOpen     CircuitBreakerState = "open"
 	StateClosed   CircuitBreakerState = "closed"
 	StateHalfOpen CircuitBreakerState = "half-open"
 )
-
-type config struct {
-	clock clock.Clock
-
-	resetTimeout             time.Duration
-	failureRate              float32
-	numberOfCallsInHalfState int32
-	hooks                    []Hook
-}
-
-func defaultConfig() *config {
-	return &config{
-		clock:                    clock.New(),
-		resetTimeout:             60000 * time.Millisecond,
-		failureRate:              0.5,
-		numberOfCallsInHalfState: 5,
-	}
-}
-
-type option func(*config)
-
-func WithClock(clock clock.Clock) option {
-	return func(c *config) {
-		c.clock = clock
-	}
-}
-
-func WithResetTimeout(d time.Duration) option {
-	return func(c *config) {
-		c.resetTimeout = d
-	}
-}
-
-func WithFailureRate(p float32) option {
-	return func(c *config) {
-		c.failureRate = p
-	}
-}
-
-func WithNumberOfCallsInHalfState(n int32) option {
-	return func(c *config) {
-		c.numberOfCallsInHalfState = n
-	}
-}
-
-func WithHook(h Hook) option {
-	return func(c *config) {
-		c.hooks = append(c.hooks, h)
-	}
-}
 
 type CircuitBreakerState string
 
